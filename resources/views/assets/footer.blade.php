600b69f809f071276bf6e539ac50b72c9bf6ab65
julia eka <eka47577@gmail.com>
2022-09-18 10:49:21 +0700

    templating yoss

---
+++ resources/views/assets/footer.blade.php
@@ -0,0 +1,9 @@
+
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2022 <div class="bullet"></div> Yoss-App V.1</a>
+        </div>
+        <div class="footer-right">
+          
+        </div>
+      </footer>
