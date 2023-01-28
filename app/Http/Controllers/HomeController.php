01a1201cb25feec6d54dd982985e7a9d0375a7a2
julia eka <eka47577@gmail.com>
2023-01-28 23:53:17 +0700

    fix chart sales

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,16 +19,44 @@ class HomeController extends Controller
             ->count();
 
         $count_product = Product::count();
-        $count_sales =  DB::table('detail_transactions')
+        $count_salesday =  DB::table('detail_transactions')
             ->selectRaw('SUM(products.price) as total_price')
             ->leftJoin('products', 'products.id', '=', 'detail_transactions.product_id')
         ->whereDate('detail_transactions.created_at', Carbon::today())
             ->first()->total_price;
 
-        $pendapatan = number_format($count_sales, 0, ',', '.');
+        $pendapatanHarian = number_format($count_salesday, 0, ',', '.');
+
+        $startOfWeek = Carbon::now()->startOfWeek();
+        $endOfWeek = Carbon::now()->endOfWeek();
+        $count_salesweek =  DB::table('detail_transactions')
+        ->selectRaw('SUM(products.price) as total_price')
+        ->leftJoin('products', 'products.id', '=', 'detail_transactions.product_id')
+        ->whereBetween('detail_transactions.created_at', [$startOfWeek, $endOfWeek])
+            ->first()->total_price;
+
+        $pendapatanMingguan = number_format($count_salesweek, 0, ',', '.');
+
+        $count_salesmonth =  DB::table('detail_transactions')
+        ->selectRaw('SUM(products.price) as total_price')
+        ->leftJoin('products', 'products.id', '=', 'detail_transactions.product_id')
+        ->whereMonth('detail_transactions.created_at', Carbon::now()->month)
+            ->first()->total_price;
+
+        $pendapatanBulanan = number_format($count_salesmonth, 0, ',', '.');
+
+        $count_salesYear =  DB::table('detail_transactions')
+        ->selectRaw('SUM(products.price) as total_price')
+        ->leftJoin('products', 'products.id', '=', 'detail_transactions.product_id')
+        ->whereYear('detail_transactions.created_at', Carbon::now()->year)
+            ->first()->total_price;
+
+        $pendapatanTahunan = number_format($count_salesYear, 0, ',', '.');
+        // dd($count_salesmonth, $count_salesYear);
 
         $count_users = User::count();
 
+        $hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
         $Salesofday = DB::table('detail_transactions')
         ->select(DB::raw('DATE_FORMAT(detail_transactions.created_at, "%W") as hari, SUM(products.price) as count'))
         ->leftJoin('products', 'products.id', '=', 'detail_transactions.product_id')
@@ -46,6 +74,16 @@ class HomeController extends Controller
         $datasets = json_encode($datasets);
 
         // dd($labels, $datasets);
-        return view('welcome', compact('count_transaction', 'count_product', 'pendapatan', 'count_users', 'labels', 'datasets'));
+        return view('welcome', compact(
+            'count_transaction',
+            'count_product',
+            'pendapatanHarian',
+            'pendapatanMingguan',
+            'pendapatanBulanan',
+            'pendapatanTahunan',
+            'count_users',
+            'labels',
+            'datasets'
+        ));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,6 +25,7 @@
           </div>
         </div>
       </div>
+
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
           <div class="card-icon bg-warning">
@@ -35,7 +36,7 @@
               <h4>Pendapatan</h4>
             </div>
             <div class="card-body">
-              {{ $pendapatan }}
+              Rp. {{ $pendapatanHarian }}
             </div>
           </div>
         </div>
@@ -76,30 +77,26 @@
       <div class="col-lg-8">
         <div class="card">
           <div class="card-header">
-            <h4>Statistics</h4>
+            <h4>Contoh Penjualan perbulan</h4>
           </div>
           <div class="card-body">
             <canvas id="mySales" height="158"></canvas>
             <div class="statistic-details mt-sm-4">
               <div class="statistic-details-item">
-                <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 7%</span>
-                <div class="detail-value">$243</div>
-                <div class="detail-name">Today's Sales</div>
+                <div class="detail-value">Rp. {{ $pendapatanHarian }} </div>
+                <div class="detail-name">Perhari</div>
               </div>
               <div class="statistic-details-item">
-                <span class="text-muted"><span class="text-danger"><i class="fas fa-caret-down"></i></span> 23%</span>
-                <div class="detail-value">$2,902</div>
-                <div class="detail-name">This Week's Sales</div>
+                <div class="detail-value">Rp. {{ $pendapatanMingguan }}</div>
+                <div class="detail-name">Perminggu</div>
               </div>
               <div class="statistic-details-item">
-                <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span>9%</span>
-                <div class="detail-value">$12,821</div>
-                <div class="detail-name">This Month's Sales</div>
+                <div class="detail-value">Rp. {{ $pendapatanBulanan }}</div>
+                <div class="detail-name">Perbulan</div>
               </div>
               <div class="statistic-details-item">
-                <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 19%</span>
-                <div class="detail-value">$92,142</div>
-                <div class="detail-name">This Year's Sales</div>
+                <div class="detail-value">Rp. {{ $pendapatanTahunan }}</div>
+                <div class="detail-name">Pertahun</div>
               </div>
             </div>
           </div>
@@ -110,49 +107,51 @@
 </div>
 <!-- Main Content -->
 <script>
-  var labels = <?= json_encode($labels) ?>;
-  var data = <?= json_encode($datasets) ?>;
-  // console.log(labels, data)
-  "use strict";
+  var labels = <?= $labels ?>;
+  var data = <?= $datasets ?>;
+  console.log(labels, data)
 
-  var statistics_chart = document.getElementById("mySales").getContext('2d');
-  var mySales = new Chart(statistics_chart, {
-    type: 'line',
-    data: {
-      labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-      datasets: [{
-        label: 'Statistics',
-        data: [640, 387, 530, 302, 430, 270, 488],
-        borderWidth: 5,
-        borderColor: '#6777ef',
-        backgroundColor: 'transparent',
-        pointBackgroundColor: '#fff',
-        pointBorderColor: '#6777ef',
-        pointRadius: 4
-      }]
-    },
-    options: {
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          ticks: {
-            stepSize: 150
-          }
-        }],
-        xAxes: [{
-          gridLines: {
-            color: '#fbfbfb',
-            lineWidth: 2
-          }
+  "use strict";
+  window.onload = function() {
+    var statistics_chart = document.getElementById("mySales").getContext('2d');
+    var mySales = new Chart(statistics_chart, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Statistics',
+          data: data,
+          borderWidth: 5,
+          borderColor: '#6777ef',
+          backgroundColor: 'transparent',
+          pointBackgroundColor: '#fff',
+          pointBorderColor: '#6777ef',
+          pointRadius: 4
         }]
       },
-    }
-  });
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              stepSize: 150
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              color: '#fbfbfb',
+              lineWidth: 2
+            }
+          }]
+        },
+      }
+    });
+  }
 </script>
 @include('assets.footer')
